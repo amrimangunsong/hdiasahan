dd2498ce2a6f2448c965fee5dc5e62794248ca8a
Amri Maslihan Mangunsong <4mr3yer@gmail.com>
2026-06-18 10:17:34 +0700

    feat: implement code obfuscation for production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import obfuscator from 'vite-plugin-javascript-obfuscator'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    obfuscator({
+      include: [/\.(jsx?|tsx?|cjs|mjs)$/],
+      exclude: [/node_modules/],
+      apply: 'build',
+      options: {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+        numbersToExpressions: true,
+        simplify: true,
+        stringArray: true,
+        stringArrayThreshold: 0.75,
+      }
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
